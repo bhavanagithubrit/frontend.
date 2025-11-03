883636462f976da15339610295d0f0d256985d5b
bhavanagithubrit <bhavana25846@gmail.com>
2025-11-03 12:27:59 +0530

    commit sentence

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// alert("working")
+// variables
+// js is untyped language
+//  can create using-var,let,cont
+// variable is declared and intialization
+var c//declaration
+var a =10
+var b=20
+a=20
+document.writeln("<h1>",a+b,"</h1>")
+document.writeln("<h1>","c","</h1>")
+document.writeln("<h1>","d","</h1>")
